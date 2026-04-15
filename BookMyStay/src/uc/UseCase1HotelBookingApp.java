e0504c83851c643df0d8df63adfeaf034c36166b
Jayanth <st2698@srmist.edu.in>
2026-04-15 19:05:38 +0530

    [jayanth] Added feature/UC1-WelcomePage

---
+++ BookMyStay/src/uc/UseCase1HotelBookingApp.java
@@ -0,0 +1,15 @@
+/**
+ * BookMyStay - Hotel Booking Management System
+ * 
+ * Entry point for the application.
+ * Demonstrates startup and welcome message.
+ * 
+ * @author Sri
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Hotel Booking System v1.0");
+    }
+}
